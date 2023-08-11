ba3fa794adf12277d0b61f24aa91105cd2eafb32
Kgoerbi <115283973+Kgoerbi@users.noreply.github.com>
2023-08-11 13:59:11 +0200

    Delete test.py
    It was a useless file.

--- fs2_to_fs4_airport_converter/python_scripts/test.py
+++
@@ -1,8 +0,0 @@
-line_combinations  = [
-        [0,1,2,3,4,5],
-        [6,7,8,9,10]          
-                          ]
-
-for row in line_combinations:
-    for column in row:
-         print(column)
