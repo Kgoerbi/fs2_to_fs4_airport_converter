7a58d1cc0755f3d36e4db2c09aae96995f5bcc8d
Kgoerbi <115283973+Kgoerbi@users.noreply.github.com>
2023-08-11 13:54:49 +0200

    Update main.py
    I added the delete.py file to the main.py, that way the auto generated .DS_store file in MacOS get deleted to prevent further complications.

--- fs2_to_fs4_airport_converter/python_scripts/main.py
+++ fs2_to_fs4_airport_converter/python_scripts/main.py
@@ -1,20 +1,20 @@
 import os
 import tsc_to_tap
 import get_lon_lat
-
+import delete
 
 output_folder_path = ('../output/')
 input_folder_path = ('../input/')
 input_folder_path_list = os.listdir(input_folder_path)
 output_file_name = input_folder_path_list[0]
 
+delete.delete_DS_store(input_folder_path + output_file_name + '/')
+
 input_tsc_path = input_folder_path + output_file_name + '/' + output_file_name + '.tsc'
 input_toc_path = input_folder_path + output_file_name + '/' + output_file_name + '.toc'
 
 lon = float(get_lon_lat.get_lon(input_tsc_path))
 lat = float(get_lon_lat.get_lat(input_tsc_path))
-
-
 
 with open('../files/tap-file_template.tap','r') as tap_file_template:          #get template
     tap_template = tap_file_template.read()
