3e7021c65a9bf9680c42dc1c40c31afce35dca91
Kgoerbi <115283973+Kgoerbi@users.noreply.github.com>
2023-08-21 15:25:04 +0200

    Update main.py

--- fs2_to_fs4_airport_converter/python_scripts/main.py
+++ fs2_to_fs4_airport_converter/python_scripts/main.py
@@ -4,6 +4,17 @@ import toc_to_tap
 import get_lon_lat
 
 
+def remove_tabs(file):
+    with open(file, 'r') as file1:
+        a = file1.readlines()
+        file1.close()
+    
+    with open(file, 'w') as file2:
+        b = [item.replace('	', '    ') for item in a]
+        file2.writelines(b)
+        file2.close()
+
+
 output_folder_path = ('../output/')
 input_folder_path = ('../input/')
 input_folder_path_list = os.listdir(input_folder_path)
@@ -39,9 +50,11 @@ for file in input_folder_path_list:
             output_file.write(tap_template)
             output_file.close()
 
-        tsc_to_tap.cp_general_information(input_tsc_path, output_folder_path + output_file_name + '.tap')
-        toc_to_tap.convert_xref(input_toc_path,output_folder_path + output_file_name + '.tap')
-        tsc_to_tap.convert_boundaries(lon,lat,output_folder_path + output_file_name + '.tap')
-        tsc_to_tap.convert_helipads(input_tsc_path,output_folder_path + output_file_name + '.tap')
-        tsc_to_tap.convert_runway_pairs(input_tsc_path,output_folder_path + output_file_name + '.tap')
-        tsc_to_tap.convert_parking_positions(input_tsc_path,output_folder_path + output_file_name + '.tap')
+            tsc_to_tap.cp_general_information(input_tsc_path, output_folder_path + output_file_name + '.tap')
+            toc_to_tap.convert_xref(input_toc_path,output_folder_path + output_file_name + '.tap')
+            tsc_to_tap.convert_boundaries(lon,lat,output_folder_path + output_file_name + '.tap')
+            tsc_to_tap.convert_helipads(input_tsc_path,output_folder_path + output_file_name + '.tap')
+            tsc_to_tap.convert_runway_pairs(input_tsc_path,output_folder_path + output_file_name + '.tap')
+            tsc_to_tap.convert_parking_positions(input_tsc_path,output_folder_path + output_file_name + '.tap')
+            
+            remove_tabs(output_folder_path + output_file_name + '.tap')
