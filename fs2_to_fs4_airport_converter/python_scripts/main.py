a3734dac7dee43ff0a8ee56f66aef69608a549cc
Kgoerbi <115283973+Kgoerbi@users.noreply.github.com>
2023-08-25 17:23:51 +0200

    Update main.py

--- fs2_to_fs4_airport_converter/python_scripts/main.py
+++ fs2_to_fs4_airport_converter/python_scripts/main.py
@@ -3,7 +3,6 @@ import tsc_to_tap
 import toc_to_tap
 import get_lon_lat
 
-#def to replace all "tabs" with four spaces
 def remove_tabs(file):
     with open(file, 'r') as file1:
         a = file1.readlines()
@@ -14,43 +13,50 @@ def remove_tabs(file):
         file2.writelines(b)
         file2.close()
 
-#here you define your input/output folder path,if you have changed it
+
 output_folder_path = ('../output/')
 input_folder_path = ('../input/')
 input_folder_path_list = os.listdir(input_folder_path)
 
 for file in input_folder_path_list:
-    if len(file) == 4 or len(file) == 5 or len(file) == 6:
-        output_file_name = file
-    
-        try:
-            x = ('.DS_Store')
-            y = os.path.join(input_folder_path, x)
+    try:
+        if len(file) == 4 or len(file) == 6:
+            output_file_name = file
+        
+            try:
+                x = ('.DS_Store')
+                y = os.path.join(input_folder_path, x)
 
-            os.remove(y)
-        except Exception as e:
-            print(e)
+                os.remove(y)
+            except Exception as e:
+                print(e)
 
-        input_tsc_path = input_folder_path + output_file_name + '/' + output_file_name + '.tsc'
-        input_toc_path = input_folder_path + output_file_name + '/' + output_file_name + '.toc'
+            input_tsc_path = input_folder_path + output_file_name + '/' + output_file_name + '.tsc'
 
-        lon = float(get_lon_lat.get_lon(input_tsc_path))
-        lat = float(get_lon_lat.get_lat(input_tsc_path))
 
-        with open('../files/tap-file_template.tap','r') as tap_file_template:          #get template
-            tap_template = tap_file_template.read()
-            tap_file_template.close()
 
-        with open(output_folder_path + output_file_name + '.tap', 'w+') as output_file:      #create .tap-file
-            
-            output_file.write(tap_template)
-            output_file.close()
+            input_toc_path = input_folder_path + output_file_name + '/' + output_file_name + '.toc'
 
-            tsc_to_tap.cp_general_information(input_tsc_path, output_folder_path + output_file_name + '.tap')
-            toc_to_tap.convert_xref(input_toc_path,output_folder_path + output_file_name + '.tap')
-            tsc_to_tap.convert_boundaries(lon,lat,output_folder_path + output_file_name + '.tap')
-            tsc_to_tap.convert_helipads(input_tsc_path,output_folder_path + output_file_name + '.tap')
-            tsc_to_tap.convert_runway_pairs(input_tsc_path,output_folder_path + output_file_name + '.tap')
-            tsc_to_tap.convert_parking_positions(input_tsc_path,output_folder_path + output_file_name + '.tap')
-            
-            remove_tabs(output_folder_path + output_file_name + '.tap')
+            lon = float(get_lon_lat.get_lon(input_tsc_path))
+            lat = float(get_lon_lat.get_lat(input_tsc_path))
+
+
+            with open('../files/tap-file_template.tap','r') as tap_file_template:          #get template
+                tap_template = tap_file_template.read()
+                tap_file_template.close()
+
+            with open(output_folder_path + output_file_name + '.tap', 'w+') as output_file:      #create .tap-file
+                
+                output_file.write(tap_template)
+                output_file.close()
+
+                tsc_to_tap.cp_general_information(input_tsc_path, output_folder_path + output_file_name + '.tap')
+                toc_to_tap.convert_xref(input_toc_path,output_folder_path + output_file_name + '.tap')
+                tsc_to_tap.convert_boundaries(lon,lat,output_folder_path + output_file_name + '.tap')
+                tsc_to_tap.convert_helipads(input_tsc_path,output_folder_path + output_file_name + '.tap')
+                tsc_to_tap.convert_runway_pairs(input_tsc_path,output_folder_path + output_file_name + '.tap')
+                tsc_to_tap.convert_parking_positions(input_tsc_path,output_folder_path + output_file_name + '.tap')
+                
+                remove_tabs(output_folder_path + output_file_name + '.tap')
+    except Exception as e:
+        print(output_file_name + ': '+ e)
